993eb10b26aa16e929e244bd74ca6e2b7f09c068
byzpylot0 <bordereau_reste_0h@icloud.com>
2024-07-16 23:52:54 +0200

    Server start

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-
 const express = require("express");
-const app = express(); 
-const PORT = 3000; 
+const app = express();
+const port = 3000;
 
-app.use()
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Le serveur Express a démaré !");
+});
+
+app.listen(port, () => {
+  console.log(`Le serveur est entrain de tourné sur le port : ${port}`);
+});
