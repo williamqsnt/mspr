2442c0baefe99927a5939f33169817e640ad125a
thomas.deschampt@epsi.fr <thomas.deschampt@epsi.fr>
2024-07-17 20:31:21 +0200

    correction de la table utilisateur et mise en place de l'ajout d'utilisateur

--- controllers/Utilisateur/ajouter.js
+++ controllers/Utilisateur/ajouter.js
@@ -22,33 +22,44 @@ const ajouter = async (req, res) => {
   let salt = bcrypt.genSaltSync(10);
 
   try {
-    const { nom, prenom, dateNaissance, motDePasse, numero, email, adresse} = req.query;
+    const { nom, prenom, dateNaissance, motDePasse, numero, email, adresse, pseudo} = req.query;
 
     const saltRounds = 10;
     const hashedPassword = await bcrypt.hash(motDePasse, saltRounds);
     const encryptedNom = encrypt(nom);
     const encryptedPrenom = encrypt(prenom);
-    const encryptedDateDeNaissance = encrypt(dateNaissance.toString());
+    const encryptedDateDeNaissance = encrypt(dateNaissance);
     const encryptedNum = encrypt(numero);
     const encryptedEmail = encrypt(email);
     const encryptedAdresse = encrypt(adresse);
 
+        // Log the lengths of the encrypted fields
+        console.log("Length of encryptedNom: ", encryptedNom.length);
+        console.log("Length of encryptedPrenom: ", encryptedPrenom.length);
+        console.log("Length of encryptedDateDeNaissance: ", encryptedDateDeNaissance.length);
+        console.log("Length of encryptedNum: ", encryptedNum.length);
+        console.log("Length of encryptedEmail: ", encryptedEmail.length);
+        console.log("Length of encryptedAdresse: ", encryptedAdresse.length);
+        console.log("Length of pseudo: ", pseudo.length);
+        console.log("Length of hashedPassword: ", hashedPassword.length);
+
       const newUtilisateur = await prisma.utilisateur.create({
         data: {
-            encryptedNom,
-            encryptedPrenom,
-            encryptedDateDeNaissance,
-            encryptedNum,
-            encryptedEmail,
-            encryptedAdresse,
+            nom : encryptedNom,
+            prenom : encryptedPrenom,
+            dateNaissance : encryptedDateDeNaissance,
+            numero : encryptedNum,
+            email : encryptedEmail,
+            adresse : encryptedAdresse,
             pseudo,
-            hashedPassword
+            motDePasse : hashedPassword
         },
       });
       res.status(200).json({ message: "Utilisateur créé" });
     } catch (error) {
     res.status(500).json({
       error: "Erreur lors de la création de l'utilisateur",
+      error: error.message
     });
   } finally {
     await prisma.$disconnect();
