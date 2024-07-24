8c92c3f4a2b89ddbb7d43a4594995e901d75e73a
thomas.deschampt@epsi.fr <thomas.deschampt@epsi.fr>
2024-07-24 21:17:28 +0200

    modification des api

--- controllers/Plante/recupererLocalisation.js
+++ controllers/Plante/recupererLocalisation.js
@@ -16,6 +16,7 @@ const recupererLocalisation = async (req, res) => {
             },
             select: {
                 adresse: true,
+                idPlante: true
             },
         });
 
