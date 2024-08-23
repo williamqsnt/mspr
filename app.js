16f82e2b6ebbc7c8edd3d1e301d368b58de779e4
thomas.deschampt@epsi.fr <thomas.deschampt@epsi.fr>
2024-08-23 16:23:04 +0200

    mise en place des socket io

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
+const http = require("http"); 
+const { Server } = require("socket.io"); // Importation de Socket.IO
 const port = 3000;
 const cors = require("cors");
 const setupSwagger = require('./swagger');
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
+  }
+});
 
 app.use(cors());
 app.use(express.json());
@@ -20,6 +35,37 @@ app.get("/", (req, res) => {
 
 setupSwagger(app);
 
-app.listen(port, () => {
-  console.log(`Le serveur est entrain de tourné sur le port : ${port}`);
+// Configuration de Socket.IO
+io.on("connection", (socket) => {
+  console.log("Un utilisateur est connecté");
+
+  // Écouter un événement pour récupérer les messages d'une conversation spécifique
+  socket.on("recupererMessages", async (idConversation) => {
+    try {
+      // Appel à l'API Prisma pour récupérer les messages
+      const messages = await prisma.message.findMany({
+        where: {
+          idConversation: parseInt(idConversation, 10),
+        },
+        orderBy: {
+          dateEnvoi: 'asc',
+        },
+      });
+
+      // Envoi des messages au client qui a fait la requête
+      socket.emit("messagesRecus", messages);
+    } catch (error) {
+      console.error("Erreur lors de la récupération des messages:", error.message);
+      socket.emit("erreur", "Erreur lors de la récupération des messages");
+    }
+  });
+
+    // Gestion de la déconnexion
+    socket.on("disconnect", () => {
+      console.log("Un utilisateur s'est déconnecté");
+    });
+  })
+
+server.listen(port, () => {
+  console.log(`Le serveur est en train de tourner sur le port : ${port}`);
 });
